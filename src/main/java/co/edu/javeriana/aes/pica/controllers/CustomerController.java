1369292ca2ac2a2c9d3a9b8ad5960497dfbcf82f
Sebastian Pacheco <sebastianpacheco@MacBook-Pro-de-Sebastian.local>
2017-03-20 11:41:20 -0500

    first commit

---
+++ src/main/java/co/edu/javeriana/aes/pica/controllers/CustomerController.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.javeriana.aes.pica.controllers;
+
+import co.edu.javeriana.aes.pica.model.Customer;
+import co.edu.javeriana.aes.pica.model.CustomerWrapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author sebastianpacheco
+ */
+@Controller
+public class CustomerController {
+    
+    @RequestMapping(value = "/customer" , method = RequestMethod.GET, produces = "application/json")
+    public @ResponseBody ResponseEntity<CustomerWrapper> getCustomer(){
+        CustomerWrapper wrapper = new CustomerWrapper();
+        Customer cust = new Customer();
+        cust.setCustomerId(1);
+        cust.setCustomerFirstName("Jon");
+        cust.setCustomerLastName("Doe");
+        cust.setCustomerAdress("FakeStreet 123");
+        cust.setCustomerEmail("jon.dow@test.com");
+        cust.setCustomerPhone("222-33-444");
+        cust.setCustomerCreditCardType("Visa");
+        cust.setCustomerCreditCardNumber("33-44-22-11-22");
+        cust.setCustomerStatus("1");
+        wrapper.setCustomer(cust);
+        return new ResponseEntity<>(wrapper,HttpStatus.OK);
+    }
+    
+    @RequestMapping(value = "/customer" , method = RequestMethod.POST, produces = "application/json")
+    public @ResponseBody ResponseEntity<CustomerWrapper> createCustomer(@RequestBody Customer c){
+        CustomerWrapper wrapper = new CustomerWrapper();
+        c.setCustomerFirstName(c.getCustomerFirstName()+"Created");
+        wrapper.setCustomer(c);
+        return new ResponseEntity<>(wrapper,HttpStatus.OK);
+    }
+    
+}
---
+++ src/main/java/co/edu/javeriana/aes/pica/model/CustomerWrapper.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.javeriana.aes.pica.model;
+
+/**
+ *
+ * @author sebastianpacheco
+ */
+public class CustomerWrapper {
+    
+    private Customer customer;
+
+    /**
+     * @return the customer
+     */
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    /**
+     * @param customer the customer to set
+     */
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+    
+    
+    
+}
